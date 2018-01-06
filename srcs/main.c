3401e3547aa90305020eb0606b0886a0ac32f660
Mathilde RESSIER <ressier@stg-interactive.com>
2018-01-06 10:25:38 +0100

    main test

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,21 @@
 #include "libft.h"
+#include "game_2048.h"
 
 int		main()
 {
-	return (0);
+    t_grid      new_grid;
+
+    if (t_grid_init(&new_grid, GRID_SIZE_MAX) == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+
+    for (int i = 0; i < GRID_SIZE_MAX; i++)
+    {
+
+    }
+
+    if (t_grid_set_number(&new_grid, GRID_SIZE_MAX - 1, GRID_SIZE_MAX - 1, 1) == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+    t_grid_display(new_grid);
+	return (EXIT_SUCCESS);
 }
 
