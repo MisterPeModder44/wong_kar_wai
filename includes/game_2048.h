364fba5fbb1936d678ac358bda7622309868a679
Mathilde RESSIER <ressier@stg-interactive.com>
2018-01-06 13:50:56 +0100

    include file

--- includes/game_2048.h
+++ includes/game_2048.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   2048.h                                             :+:      :+:    :+:   */
+/*   game_2048.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mressier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/06 09:52:38 by mressier          #+#    #+#             */
-/*   Updated: 2018/01/06 09:53:20 by mressier         ###   ########.fr       */
+/*   Created: 2018/01/06 13:44:38 by mressier          #+#    #+#             */
+/*   Updated: 2018/01/06 13:44:40 by mressier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,62 +17,60 @@
 
 # define GRID_SIZE_MAX   4
 
-enum e_const
+enum				e_const
 {
-    EMPTY_VALUE = 0,
-    WIN_VALUE = 2048
+	EMPTY_VALUE = 0,
+	WIN_VALUE = 2048
 };
 
-typedef enum        e_move
+typedef enum		e_move
 {
-    MOVE_UP = 0,
-    MOVE_RIGHT,
-    MOVE_DOWN,
-    MOVE_LEFT
-}                   t_move;
+	MOVE_UP = 0,
+	MOVE_RIGHT,
+	MOVE_DOWN,
+	MOVE_LEFT
+}					t_move;
 
-typedef enum        e_square_action
+typedef enum		e_merge_turns
 {
-    SQUARE_MOVE = 0,
-    SQUARE_MERGE,
-    SQUARE_BLOCKED,
-    SQUARE_NOTHING,
-    SQUARE_ERROR
-}                   t_square_action;
+	TURN_MOVE = 0,
+	TURN_MERGE,
+	TURN_RE_MOVE,
+	TURN_MAX
+}					t_merge_turns;
 
-typedef struct      s_grid
+typedef struct		s_grid
 {
-	unsigned int    grid[GRID_SIZE_MAX][GRID_SIZE_MAX];
-	unsigned int    grid_size;
-}                   t_grid;
+	unsigned int	grid[GRID_SIZE_MAX][GRID_SIZE_MAX];
+	unsigned int	grid_size;
+}					t_grid;
 
-typedef void        (*t_grid_func)(t_grid *, unsigned int line,
-                                   unsigned int col);
+typedef	void		(*t_grid_func)(t_grid *, unsigned int line,
+											unsigned int col);
 
 /*
- * t_grid.c
- */
-void                t_grid_display(t_grid grid);
-int                 t_grid_init(t_grid *new_grid, unsigned int size);
-int                 t_grid_set_number(t_grid *grid, unsigned int line,
-									  unsigned int col, unsigned int number);
+** t_grid.c
+*/
+void				t_grid_display(t_grid grid);
+int					t_grid_init(t_grid *new_grid, unsigned int size);
+int					t_grid_set_number(t_grid *grid, unsigned int line,
+									unsigned int col, unsigned int number);
 
-void                t_grid_display_cell(t_grid *grid, unsigned int line,
-                                        unsigned int col);
-
-/*
- * t_grid_move.c
- */
-int                 t_grid_move(t_move move, t_grid *grid);
-
+void				t_grid_display_cell(t_grid *grid, unsigned int line,
+										unsigned int col);
 
 /*
- * t_grid_tools.c
- */
-bool                t_grid_is_valid_coord(t_grid grid, unsigned int line,
-                                          unsigned int col);
-unsigned int        t_grid_cell_value(t_grid grid, unsigned int line,
-                                      unsigned int col);
-unsigned int        t_grid_get_next_value(unsigned int current_value);
+** t_grid_move.c
+*/
+int					t_grid_move(t_move move, t_grid *grid);
+
+/*
+** t_grid_tools.c
+*/
+bool				t_grid_is_valid_coord(t_grid grid, unsigned int line,
+											unsigned int col);
+unsigned int		t_grid_cell_value(t_grid grid, unsigned int line,
+										unsigned int col);
+unsigned int		t_grid_get_next_value(unsigned int current_value);
 
 #endif
