7dfe8fc098731fa9fb3e4b760d1147db2840ec3b
Mathilde RESSIER <ressier@stg-interactive.com>
2018-01-06 14:15:07 +0100

    main

--- srcs/main.c
+++ srcs/main.c
@@ -3,21 +3,21 @@
 
 int		main()
 {
-    t_grid      new_grid;
+	t_grid	  new_grid;
 
-    if (t_grid_init(&new_grid, GRID_SIZE_MAX) == EXIT_FAILURE)
-        return (EXIT_FAILURE);
+	if (t_grid_init(&new_grid, GRID_SIZE_MAX) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 
-    for (int i = 0; i < 10; ++i) {
-        t_grid_set_number(&new_grid, 0, 0, 1);
-        t_grid_display(new_grid);
-        ft_putchar('\n');
+	for (int i = 0; i < 10; ++i) {
+		t_grid_set_number(&new_grid, 0, 0, 1);
+		t_grid_display(new_grid);
+		ft_putchar('\n');
 
-        ft_putstr("MOVE DOWN\n");
-        t_grid_move(MOVE_DOWN, &new_grid);
-        t_grid_display(new_grid);
-        ft_putchar('\n');
-    }
+		ft_putstr("MOVE RIGHT\n");
+		t_grid_move(MOVE_RIGHT, &new_grid);
+		t_grid_display(new_grid);
+		ft_putchar('\n');
+	}
 
 	return (EXIT_SUCCESS);
 }
