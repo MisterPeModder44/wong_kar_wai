dba7148fe0aaf49728bf36e48127d6cc1e365e56
Yanis GUAYE <yguaye@student.42.fr>
2018-01-07 17:40:54 +0100

    Fix the norm.

--- srcs/score_events.c
+++ srcs/score_events.c
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/07 15:34:04 by yguaye            #+#    #+#             */
-/*   Updated: 2018/01/07 17:23:05 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/01/07 17:34:16 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,16 @@ void			score_redraw(t_gamestate *state)
 	unsigned int	i;
 	t_score			*score_tab;
 
-	win = subwin(stdscr, state->scores->nb_score, 29, LINES / 2 - state->scores->nb_score / 2, COLS / 2 - 14);
+	win = subwin(stdscr, state->scores->nb_score, 29, LINES / 2 -
+			state->scores->nb_score / 2, COLS / 2 - 14);
 	i = -1;
 	score_tab = state->scores->score_tab;
 	while (++i < state->scores->nb_score)
-		mvwprintw(win, i + 1, 1, "%d - %s: %d", i + 1, score_tab[i].name, score_tab[i].score);
+		mvwprintw(win, i + 1, 1, "%d - %s: %d", i + 1, score_tab[i].name,
+				score_tab[i].score);
 	box(win, ACS_VLINE, ACS_HLINE);
 	delwin(win);
 	print_middle(stdscr, LINES / 2 - state->scores->nb_score / 2 - 1, "SCORES");
-	print_middle(stdscr, LINES / 2 + state->scores->nb_score / 2 + 1, "[ BACK ]");
+	print_middle(stdscr, LINES / 2 + state->scores->nb_score / 2 + 1,
+			"[ BACK ]");
 }
